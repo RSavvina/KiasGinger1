d431543bb7f95fcb05b5f315046022bbcc358d92
RSavvina <savvina.rousou@amdocs.com>
2023-07-28 23:09:33 +0300

    new solutions

---
+++ sims_update_queries.sql
@@ -0,0 +1,3 @@
+update serial_item_inv set pin1='1234'  where IMSI= '262020451005735'
+update serial_item_inv set puk2='199919'  where IMSI= '262020451005735'
+update serial_item_inv set  ki='SAVVINA1234567890' where IMSI= '262020451005735';
